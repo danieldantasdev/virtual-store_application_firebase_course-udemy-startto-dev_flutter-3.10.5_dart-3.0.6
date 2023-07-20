b2f91f2e8916ea5a85efe95734442b65247cb958
danieldantasdev <contatolobinhodev@gmail.com>
2023-07-20 12:43:45 -0300

    feat: implements button logout

--- lib/widgets/drawers/custom_drawer.widget.dart
+++ lib/widgets/drawers/custom_drawer.widget.dart
@@ -8,20 +8,21 @@ class CustomDrawerWidget extends StatelessWidget {
 
   PageController pageController = PageController();
 
+  Widget buildDrawerBackground() => Container(
+        decoration: const BoxDecoration(
+          gradient: LinearGradient(
+            colors: [
+              Color.fromARGB(255, 203, 236, 241),
+              Color.fromARGB(255, 255, 255, 255),
+            ],
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+          ),
+        ),
+      );
+
   @override
   Widget build(BuildContext context) {
-    Widget buildDrawerBackground() => Container(
-          decoration: const BoxDecoration(
-            gradient: LinearGradient(
-              colors: [
-                Color.fromARGB(255, 203, 236, 241),
-                Color.fromARGB(255, 255, 255, 255),
-              ],
-              begin: Alignment.topCenter,
-              end: Alignment.bottomCenter,
-            ),
-          ),
-        );
     return Drawer(
       child: Stack(
         children: [
@@ -34,74 +35,100 @@ class CustomDrawerWidget extends StatelessWidget {
                   margin: const EdgeInsets.only(bottom: 10.0),
                   padding: const EdgeInsets.all(10.0),
                   height: 200,
-                  child: Stack(children: [
-                    const Positioned(
-                      top: 8.0,
-                      left: 0.0,
-                      child: Text(
-                        "Flutter's\nClothing",
-                        style: TextStyle(
-                          fontSize: 34.0,
-                          fontWeight: FontWeight.w900,
+                  child: Stack(
+                    children: [
+                      const Positioned(
+                        top: 8.0,
+                        left: 0.0,
+                        child: Text(
+                          "Flutter's\nClothing",
+                          style: TextStyle(
+                            fontSize: 34.0,
+                            fontWeight: FontWeight.w900,
+                          ),
                         ),
                       ),
-                    ),
-                    Positioned(
-                      left: 0.0,
-                      bottom: 0.0,
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          const Text(
-                            "Olá,",
-                            style: TextStyle(
-                              fontSize: 18.0,
-                              fontWeight: FontWeight.w900,
-                            ),
-                          ),
-                          GestureDetector(
-                            child: Text(
-                              "Entre ou Casdastre-se >",
+                      Positioned(
+                        left: 0.0,
+                        bottom: 0.0,
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            const Text(
+                              "Olá,",
                               style: TextStyle(
-                                color: Theme.of(context).primaryColor,
-                                fontSize: 16.0,
+                                fontSize: 18.0,
                                 fontWeight: FontWeight.w900,
                               ),
                             ),
-                            onTap: () {
-                              Navigator.of(context).push(MaterialPageRoute(
-                                builder: (context) => SignInScreen(),
-                              ));
-                            },
-                          )
-                        ],
-                      ),
-                    )
-                  ]),
+                            GestureDetector(
+                              child: Text(
+                                "Entre ou Cadastre-se >",
+                                style: TextStyle(
+                                  color: Theme.of(context).primaryColor,
+                                  fontSize: 16.0,
+                                  fontWeight: FontWeight.w900,
+                                ),
+                              ),
+                              onTap: () {
+                                Navigator.of(context).push(
+                                  MaterialPageRoute(
+                                    builder: (context) => SignInScreen(),
+                                  ),
+                                );
+                              },
+                            )
+                          ],
+                        ),
+                      )
+                    ],
+                  ),
                 ),
                 const Divider(),
                 DrawerTileWidget(
-                    iconData: Icons.home,
-                    text: 'Início',
-                    pageController: pageController,
-                    page: 0),
+                  iconData: Icons.home,
+                  text: 'Início',
+                  pageController: pageController,
+                  page: 0,
+                ),
                 DrawerTileWidget(
-                    iconData: Icons.list,
-                    text: 'Produtos',
-                    pageController: pageController,
-                    page: 1),
+                  iconData: Icons.list,
+                  text: 'Produtos',
+                  pageController: pageController,
+                  page: 1,
+                ),
                 DrawerTileWidget(
-                    iconData: Icons.location_on,
-                    text: 'Lojas',
-                    pageController: pageController,
-                    page: 2),
+                  iconData: Icons.location_on,
+                  text: 'Lojas',
+                  pageController: pageController,
+                  page: 2,
+                ),
                 DrawerTileWidget(
-                    iconData: Icons.playlist_add_check,
-                    text: 'Meus pedidos',
-                    pageController: pageController,
-                    page: 3),
+                  iconData: Icons.playlist_add_check,
+                  text: 'Meus pedidos',
+                  pageController: pageController,
+                  page: 3,
+                ),
               ],
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(left: 32.0, top: 700.0),
+            child: TextButton(
+              onPressed: () {
+                Navigator.of(context).pushReplacement(
+                  MaterialPageRoute(
+                    builder: (context) => SignInScreen(),
+                  ),
+                );
+              },
+              child: const Row(
+                children: [
+                  Text("Sair"),
+                  Icon(Icons.logout),
+                ],
+              ),
             ),
           )
         ],
